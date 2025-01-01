f1ba025086c69ea44b529ea0095b7357033ebda7
Abir Hasan <abirsonet563@gmail.com>
2025-01-01 04:26:11 +0000

    ES Module Part -1

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const {generateRandomNumber,celciousToFahrenheit} = require('./utils.js')
+import { getPosts } from "./postController.js";
+console.log(getPosts());
 
-console.log(`Random Number is : ${generateRandomNumber()}`);
-console.log(`Celcious To Fahrenheit : ${celciousToFahrenheit(12)}`);
+
+// const {generateRandomNumber,celciousToFahrenheit} = require('./utils.js')
+
+// console.log(`Random Number is : ${generateRandomNumber()}`);
+// console.log(`Celcious To Fahrenheit : ${celciousToFahrenheit(12)}`);
