c252d0c1d61c6b0969a873fc3903084715c94efd
Abir Hasan <abirsonet563@gmail.com>
2025-01-02 01:51:30 +0000

    Simple Routing

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res)=>{
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
+try{
+    // Check if GET request
+    if (req.method === 'GET') {
 
-    console.log(req.url);
-    console.log(req.method);
-    res.writeHead(200, {"content-type": 'text/html'});
-    res.end("<h1>Hello World</h1>");
+        if(req.url === '/') {
+            res.writeHead(200, {"Content-type": 'text/html'});
+            res.end("<h1>Home Page</h1>"); 
+         }else if (req.url === '/about'){
+            res.writeHead(200, {"Content-type": 'text/html'});
+            res.end("<h1>About Page</h1>"); 
+         }else{
+            res.writeHead(404, {"Content-type": 'text/html'});
+            res.end("<h1>Not Found</h1>"); 
+         }
+        
+    } else{
+        throw new Error('Method Not Allowed');
+    } 
+} catch (error) {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  res.end('Server Error');
+}
 });
 
 server.listen(PORT, () => {
